1d6f7f772854026b95b9ba55297d11b88c57b2f9
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2022-01-04 18:52:38 +0800

    Update Interface

---
+++ Code/PrimeNumber.c
@@ -0,0 +1,52 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool IsPrimeNumber(unsigned long int Prime) {
+        
+        if(Prime < 2) {
+                return false;
+        }
+        
+        for(unsigned long int N = 1; N < Prime; N += 1) {
+		
+		// Is not Prime Number.
+                if(!(Prime % N) && N != 1) {	
+                        return false;
+		}
+	}
+        
+        return true;
+}
+
+int* GetPrimeNumber(size_t Starting, size_t EndingIndex) {
+        
+        if(Starting > EndingIndex) {
+                return 0;
+        }
+        
+        
+        int* List = 0;
+        List = (int* )malloc(sizeof(int) * (EndingIndex - Starting));
+        
+        if (List == 0) {
+                return 0;
+        }
+        
+        
+        for(size_t Index = 0; Starting <= EndingIndex; Starting += 1) {
+                if(IsPrimeNumber(Starting)) {
+                        List[Index] = Starting;
+                        
+                        Index += 1;
+                }
+        }
+        
+        return List;
+}
+
+int main(int argc, char *argv[]) {
+
+        return 0; 
+}
---
+++ Code/PrimeNumber.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef PRIMENUMBER_H_INCLUDED
+#define PRIMENUMBER_H_INCLUDED
+
+bool IsPrimeNumber(unsigned long int Prime);
+int* GetPrimeNumber(size_t Starting, size_t EndingIndex);
+
+#endif
